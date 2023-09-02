955b5ed569df3e07492e150bc675992a77f29f57
RangaAbhiram43 <132121542+RangaAbhiram43@users.noreply.github.com>
2023-09-02 12:07:23 +0530

    Create CmdLineArgsDemoUpd.java

---
+++ CmdLineArgsDemoUpd.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class CmdLineArgsDemoUpd{
+    public static void main(String args[]){
+        if(args.length!=2){
+            System.out.println("Invalid number of arguements \n you need to pass two integers ");
+        }
+        else{
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int z = x+y;
+        System.out.println("Sum of " +x+ " and " +y+ " is " +z);
+    }
+}
+}
